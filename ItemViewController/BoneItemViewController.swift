5cf98aad635cceb0655c8a8738807acfb0ac6e27
Jean Raphael Bordet <jr.bordet@touchwa.re>
2019-04-11 10:08:50 +0200

    Initial commit

---
+++ ItemViewController/BoneItemViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BoneItemViewController.swift
+//  
+//
+//  Created by Jean Raphael Bordet on 11/04/2019.
+//
+
+import UIKit
+
+class {{ name|firstUppercase }}sViewController: UIViewController {
+    
+    // Not quite sure what this empty {{ name }} array should do in a real project , but it's here anyway to show how you can self-document your code with comments.
+    let {{ name|firstLowercase }}s: [{{ name|firstUppercase }}] = []
+    
+    func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
